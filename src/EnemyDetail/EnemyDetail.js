462424e7dd75cb0ca0f5d06d199695032d205069
antidale <antidale@gmail.com>
2023-11-08 19:47:47 -0800

    Update EnemyDetail layout
    * Removed unneeded defense values
    * speedMin and speedMax are now displayed as Speed Range
    * Combining attack info for Average Punch
    * use <details> and <summary> html elements for native open/close
    
    All this does introduce some work:
    * fix styling inside collapsable
    * move Punch and Speed info to header (possibly spell power)
    * fix issues with Script section

--- src/EnemyDetail/EnemyDetail.js
+++ src/EnemyDetail/EnemyDetail.js
@@ -5,27 +5,26 @@ import ScriptStat from '../StatItem/ScriptStat';
 import ItemTitle from '../StatItem/ItemTitle';
 
 const BossDetail = ({ enemy }) => (
-	<section className="container detail-container">
-		<span className="full-width Child title no-margin container">
-			<ItemTitle value={enemy.enemy} isTitle hideLabel />
-			<StatItem label="HP" value={enemy.hitPoints} isTitle />
-			<StatItem label="XP" value={enemy.experiencePoints} />
-		</span>
-		<StatItem label="Level" value={enemy.level} />
-		<StatItem label="Attack" value={enemy.attackPower} />
-		<StatItem label="Accuracy" value={enemy.attackPercent} />
-		<StatItem label="Attack Multi" value={enemy.attackMultiplier} />
-		<StatItem label="Spell Power" value={enemy.spellPower} />
-		<StatItem label="Max Speed" value={enemy.maxSpeed} />
-		<StatItem label="Min Speed" value={enemy.minSpeed} />
-		<StatItem label="Defense" value={enemy.defense} />
-		<StatItem label="Evade" value={enemy.evade} />
-		<StatItem label="Defense Multi" value={enemy.defenseMultiplier} />
-		<StatItem label="Magic Def" value={enemy.magicDefense} />
-		<StatItem label="Magic Evade" value={enemy.magicEvade} />
-		<StatItem label="Magic Def Multi" value={enemy.magicDefenseMultiplier} />
-		<StatItem label="Gil" value={enemy.gil} />
-		<ScriptStat values={enemy.scriptValues} />
+	<section >
+		<details className="container detail-container">
+			<summary>
+				<span className="full-width Child title no-margin container">
+					<ItemTitle value={enemy.enemy} isTitle hideLabel />
+					<StatItem label="HP" value={enemy.hitPoints} isTitle />
+					<StatItem label="XP" value={enemy.experiencePoints} />
+				</span>
+			</summary>
+			<StatItem label="Level" value={enemy.level} />
+			<StatItem label="Average Punch" value={Math.floor((enemy.attackPower * enemy.attackMultiplier * enemy.attackPercent / 100 + enemy.attackPower * enemy.attackMultiplier * enemy.attackPercent * 1.5 / 100) / 2)} />
+			<StatItem label="Speed Range" value={enemy.maxSpeed + " to " + enemy.minSpeed} />
+			<StatItem label="Spell Power" value={enemy.spellPower} />
+			<StatItem label="Max Speed" value={enemy.maxSpeed} />
+			<StatItem label="Min Speed" value={enemy.minSpeed} />
+			<StatItem label="Defense" value={enemy.defense} />
+			<StatItem label="Magic Def" value={enemy.magicDefense} />
+			<StatItem label="Gil" value={enemy.gil} />
+			<ScriptStat values={enemy.scriptValues} />
+		</details>
 	</section>
 );
 
