8ec566178b5b8ecc9c9875f497a9635b77b955cd
antidale <antidale@gmail.com>
2023-11-10 12:33:31 -0800

    Enemy Detail updates * hide script section temporarily * move to using calculation methods * have hover text explaining average punch

--- src/EnemyDetail/EnemyDetail.js
+++ src/EnemyDetail/EnemyDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './EnemyDetail.css';
 import StatItem from '../StatItem/StatItem';
-import ScriptStat from '../StatItem/ScriptStat';
+// import ScriptStat from '../StatItem/ScriptStat';
 import ItemTitle from '../StatItem/ItemTitle';
 
 const BossDetail = ({ enemy }) => (
@@ -15,17 +15,36 @@ const BossDetail = ({ enemy }) => (
 				</span>
 			</summary>
 			<StatItem label="Level" value={enemy.level} />
-			<StatItem label="Average Punch" value={Math.floor((enemy.attackPower * enemy.attackMultiplier * enemy.attackPercent / 100 + enemy.attackPower * enemy.attackMultiplier * enemy.attackPercent * 1.5 / 100) / 2)} />
-			<StatItem label="Speed Range" value={enemy.maxSpeed + " to " + enemy.minSpeed} />
+			<StatItem label="Average Punch" value={calculateDamage(enemy)} 
+					  hoverText="Does not include defenses"/>
+			<StatItem label={( isSingleSpeed(enemy)
+						? "Speed"
+						: "Speed Range")} 
+					  value={(isSingleSpeed(enemy) 
+						? enemy.maxSpeed 
+						: enemy.minSpeed + " to " + enemy.maxSpeed)} />
 			<StatItem label="Spell Power" value={enemy.spellPower} />
-			<StatItem label="Max Speed" value={enemy.maxSpeed} />
-			<StatItem label="Min Speed" value={enemy.minSpeed} />
 			<StatItem label="Defense" value={enemy.defense} />
 			<StatItem label="Magic Def" value={enemy.magicDefense} />
 			<StatItem label="Gil" value={enemy.gil} />
-			<ScriptStat values={enemy.scriptValues} />
+			{/* <ScriptStat values={enemy.scriptValues} /> */}
 		</details>
 	</section>
 );
+
+const calculateDamage = (enemy) => (
+	Math.floor((
+		//base
+		enemy.attackPower * enemy.attackMultiplier * enemy.attackPercent / 100 
+		+ 
+		// 1.5x possible multiplier
+		enemy.attackPower * enemy.attackMultiplier * enemy.attackPercent * 1.5 / 100)
+		//average them together
+		/ 2)
+)
+
+const isSingleSpeed = (enemy) => (
+	enemy.maxSpeed === enemy.minSpeed
+)
 
 export default BossDetail
