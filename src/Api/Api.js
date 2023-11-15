655f0baa4b35056c6bba48fbba986528a1ed0c77
antidale <antidale@gmail.com>
2023-11-14 20:28:54 -0800

    moving from create-react-app to vite

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,7 +2,7 @@
 
 const Api = {
 	fetchData: function (endpoint, stateFunction, paramsObject) {
-		fetch(process.env.REACT_APP_API_URL + endpoint, paramsObject)
+		fetch(import.meta.env.VITE_API_URL + endpoint, paramsObject)
 			.then(response => response.json())
 			.then(stateFunction)
 			.catch(err => console.log(err))
