4e6e9e29d25a8e2646be78f72fd8017451336577
antidale <antidale@gmail.com>
2023-11-15 23:05:16 -0800

    wip

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Summary from '../SummaryPage/Summary';
 
 const Home = () => (
 	<div>
 		<p>Welcome to a site with some helpful gameplay information about Free Enterprise</p>
+		<a href='https://wiki.ff4fe.com/doku.php'>Free Enterprise Wiki</a>
+		<Summary />
 	</div>
 )
 
