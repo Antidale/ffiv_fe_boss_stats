38ee51d51358dfa9c768d431febb5d2d05b957a1
antidale <antidale@gmail.com>
2023-11-20 18:36:49 -0800

    update name and data for summary page

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Summary from '../SummaryPage/Summary';
+import LocationSummary from '../LocationSummaryPage/LocationSummary';
 
 const Home = () => (
 	<div>
 		<p>Welcome to a site with some helpful gameplay information about Free Enterprise</p>
 		<a href='https://wiki.ff4fe.com/doku.php'>Free Enterprise Wiki</a>
-		<Summary />
+		<LocationSummary />
 	</div>
 )
 
