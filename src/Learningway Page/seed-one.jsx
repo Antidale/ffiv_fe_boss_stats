ec4755d7fcc53d26472016ae0048360da84fa807
Antidale <antidale@tellah.life>
2025-03-06 21:50:29 -0800

    trying to fix the iframe stuff

--- src/Learningway Page/seed-one.jsx
+++ src/Learningway Page/seed-one.jsx
@@ -4,7 +4,11 @@ import { useTitle } from '../Effects/SetTitle';
 const SeedOne = () => {
     useTitle("Learningway Seed One")
 
-    return (<iframe className="min-w-full min-h-dvh pt-5 bg-seedbackground" src="/seeds/Learningway_Seed_One.html"></iframe>)
+    return (
+        <article className='mx-auto bg-seedbackground'>
+            <iframe className="min-w-full min-h-dvh pt-5 bg-seedbackground" src="/seeds/Learningway_Seed_One.html" width="600" height="800" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+        </article>
+    )
 }
 
 export default SeedOne;
