fbb5e6a7e443a37dfe8980b5bd6288f5e52d0206
Antidale <antidale@tellah.life>
2025-04-02 14:24:50 -0700

    Adding my vod for Seed two

--- src/Learningway Page/LearningwayVods.js
+++ src/Learningway Page/LearningwayVods.js
@@ -9,8 +9,16 @@ const SeedOne = [
     }
 ]
 
+const SeedTwo = [
+    {
+        "runnerName": "Antidale",
+        "vodUrl": "https://www.youtube.com/watch?v=ea_L4VeLamA"
+    }
+]
+
 const LearningwayVods = {
-    "SeedOne": SeedOne
+    "SeedOne": SeedOne,
+    "SeedTwo": SeedTwo
 }
 
 export default LearningwayVods;
