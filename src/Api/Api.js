ce82ce6d9e165a0ee60fb1d793858dbdf87f7921
Antidale <antidale@tellah.life>
2025-08-02 23:07:07 -0700

    Adding stuff to display seeds by race room name

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -4,6 +4,13 @@ const Api = {
 			.then(response => response.json())
 			.then(stateFunction)
 			.catch(err => console.log(err))
+	},
+
+	fetchHtml: function (endpoint, stateFunction, paramsObject) {
+		fetch(import.meta.env.VITE_API_URL + endpoint, paramsObject)
+			.then(response => response.text())
+			.then(stateFunction)
+			.catch(err => console.log(err))
 	}
 }
 
